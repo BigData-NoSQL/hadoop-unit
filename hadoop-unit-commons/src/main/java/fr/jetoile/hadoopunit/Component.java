37b155601267932c3326c28574226ec73927ab55
jetoile <kmx.petals@gmail.com>
2017-10-29 16:59:55 +0100

    wip

--- hadoop-unit-commons/src/main/java/fr/jetoile/hadoopunit/Component.java
+++ hadoop-unit-commons/src/main/java/fr/jetoile/hadoopunit/Component.java
@@ -35,9 +35,9 @@ public enum Component {
     NEO4J("neo4j", "fr.jetoile.hadoopunit.component.Neo4jBootstrap", "neo4j.artifact"),
     KNOX("knox", "fr.jetoile.hadoopunit.component.KnoxBootstrap", "knox.artifact"),
     REDIS("redis", "fr.jetoile.hadoopunit.component.RedisBootstrap", "redis.artifact"),
-    CONFLUENT_KAFKA("confluent", "fr.jetoile.hadoopunit.component.ConfluentKafkaBootstrap", "confluent.kafka.artifact"),
-    CONFLUENT_SCHEMAREGISTRY("confluent", "fr.jetoile.hadoopunit.component.ConfluentSchemaRegistryBootstrap", "confluent.schemaregistry.artifact"),
-    CONFLUENT_KAFKA_REST("confluent", "fr.jetoile.hadoopunit.component.ConfluentKafkaRestBootstrap", "confluent.kafka_rest.artifact");
+    CONFLUENT_KAFKA("confluent_kafka", "fr.jetoile.hadoopunit.component.ConfluentKafkaBootstrap", "confluent.kafka.artifact"),
+    CONFLUENT_SCHEMAREGISTRY("confluent_schemaregistry", "fr.jetoile.hadoopunit.component.ConfluentSchemaRegistryBootstrap", "confluent.schemaregistry.artifact"),
+    CONFLUENT_KAFKA_REST("confluent_kafka_rest", "fr.jetoile.hadoopunit.component.ConfluentKafkaRestBootstrap", "confluent.kafka_rest.artifact");
 
     private String key;
     private String mainClass;
